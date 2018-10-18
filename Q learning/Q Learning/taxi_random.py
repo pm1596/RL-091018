66ba03f51d9800c831e0f0496cd92b0330755651
Pranjal Mendhekar <pm1596@yahoo.com>
2018-10-18 12:14:14 +0530

    Add files via upload

---
+++ Q learning/Q Learning/taxi_random.py
@@ -0,0 +1,24 @@
+from __future__ import print_function, division
+from builtins import range
+import time, random, math
+import numpy as np
+import gym
+
+GAME = 'Taxi-v2'
+env = gym.make(GAME)
+
+MAX_STEPS = env.spec.timestep_limit
+total_reward = 0
+
+state = env.reset()
+env.render()
+
+for step in range(MAX_STEPS):
+  action = env.action_space.sample()
+  state, reward, done, info = env.step(action)
+  total_reward += reward
+  env.render()
+  if done :
+      break
+
+print('Total reward:', total_reward)
